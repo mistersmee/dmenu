38625079f60a09c4c86a0e364179fd1487ea7629
Aseem Athale <athaleaseem@gmail.com>
2021-07-21 14:31:29 +0530

    dmenu: Apply xresources patch properly
    Signed-off-by: Aseem Athale <athaleaseem@gmail.com>

--- config.def.h
+++ config.def.h
@@ -2,9 +2,12 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
+static char font[] = "monospace:size=10";
 static const char *fonts[] = {
-	"monospace:size=10"
+	font,
+	"monospace:size=10",
 };
 static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
