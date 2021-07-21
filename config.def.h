8ab0d4d9f5ce105d5689e853845c84793370f555
Aseem Athale <athaleaseem@gmail.com>
2021-07-21 15:18:15 +0530

    Revert "dmenu: Apply xresources alt patch"
    This reverts commit 3f7e72e138a44a6d905e478a44bc9d3ee303dcdb.

--- config.def.h
+++ config.def.h
@@ -10,19 +10,14 @@ static const char *fonts[] = {
 	"monospace:size=10",
 	"JoyPixels:pixelsize=10:antialias=true:autohint=true"
 };
-static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-
-static char normfgcolor[] = "#bbbbbb";
-static char normbgcolor[] = "#222222";
-static char selfgcolor[]  = "#eeeeee";
-static char selbgcolor[]  = "#005577";
-static char *colors[SchemeLast][2] = {
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { normfgcolor, normbgcolor },
-	[SchemeSel]  = { selfgcolor,  selbgcolor  },
-	[SchemeOut]  = { "#000000",   "#00ffff" },
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeSelHighlight] = { "#ffc978", "#005577" },
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeOut] = { "#000000", "#00ffff" },
 	[SchemeOutHighlight] = { "#ffc978", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
@@ -33,15 +28,3 @@ static unsigned int lines      = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
-
-/*
- * Xresources preferences to load at startup
- */
-ResourcePref resources[] = {
-	{ "font",        STRING, &font },
-	{ "normfgcolor", STRING, &normfgcolor },
-	{ "normbgcolor", STRING, &normbgcolor },
-	{ "selfgcolor",  STRING, &selfgcolor },
-	{ "selbgcolor",  STRING, &selbgcolor },
-	{ "prompt",      STRING, &prompt },
-};
